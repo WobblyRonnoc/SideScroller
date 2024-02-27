d3dfac01df22e62a8d4ee0194d179342f1a20944
Connor MacNeil <WobblyProductions@hotmail.com>
2024-02-27 11:25:11 -0500

    formatted and added readme asset

--- Controller/player.gd
+++ Controller/player.gd
@@ -20,7 +20,6 @@ func _physics_process(delta):
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * speed
